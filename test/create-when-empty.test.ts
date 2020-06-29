196e6be39bec7c14ec3d201039ffb03e1a52189e
Gregor Martynus <gregor@martynus.net>
2020-06-29 13:33:06 -0700

    test: `options.createWhenEmpty`

---
+++ test/create-when-empty.test.ts
@@ -0,0 +1,68 @@
+import { Octokit as Core } from "@octokit/core";
+import { RequestError } from "@octokit/request-error";
+
+import { createPullRequest } from "../src";
+import { UpdateFunction } from "../src/types";
+const Octokit = Core.plugin(createPullRequest);
+
+test("options.createWhenEmpty", async () => {
+  const fixtures = require("./fixtures/create-when-empty");
+  const fixturePr = fixtures[fixtures.length - 1].response;
+  const octokit = new Octokit();
+
+  octokit.hook.wrap("request", (_, options) => {
+    const currentFixtures = fixtures.shift();
+    const {
+      baseUrl,
+      method,
+      url,
+      request,
+      headers,
+      mediaType,
+      ...params
+    } = options;
+
+    expect(
+      `${currentFixtures.request.method} ${currentFixtures.request.url}`
+    ).toEqual(`${options.method} ${options.url}`);
+
+    Object.keys(params).forEach((paramName) => {
+      expect(currentFixtures.request[paramName]).toStrictEqual(
+        params[paramName]
+      );
+    });
+
+    if (currentFixtures.response.status >= 400) {
+      throw new RequestError("Error", currentFixtures.response.status, {
+        request: currentFixtures.request,
+        headers: currentFixtures.response.headers,
+      });
+    }
+
+    return currentFixtures.response;
+  });
+
+  const updateReadme: UpdateFunction = ({ exists, encoding, content }) => {
+    if (!exists) return null;
+
+    return Buffer.from(content, encoding).toString("utf-8").toUpperCase();
+  };
+
+  const pr = await octokit.createPullRequest({
+    owner: "gr2m",
+    repo: "pull-request-test",
+    title: "Should not create a pull request",
+    head: "create-when-empty",
+    body: "",
+    createWhenEmpty: false,
+    changes: {
+      files: {
+        "test.txt": () => null,
+      },
+      commit: "empty update",
+    },
+  });
+
+  expect(pr).toStrictEqual(null);
+  expect(fixtures.length).toEqual(0);
+});
