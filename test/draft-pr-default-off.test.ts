bfdb7291b2e98adc83f2975c09e9aa9fc659f1f6
Benjamin Mangold <benjamin.mangold@rallyhealth.com>
2020-08-15 19:08:13 -0400

    new test

---
+++ test/draft-pr-default-off.test.ts
@@ -0,0 +1,51 @@
+import { Octokit as Core } from "@octokit/core";
+import { createPullRequest } from "../src";
+const Octokit = Core.plugin(createPullRequest);
+
+test("draft pr", async () => {
+  const fixtures = require("./fixtures/draft-pr");
+  const fixturePr = fixtures[fixtures.length - 1].response;
+  const octokit = new Octokit();
+
+  octokit.hook.wrap("request", (_, options) => {
+    const currentFixtures = fixtures.shift();
+    const {
+      baseUrl,
+      method,
+      url,
+      request,
+      headers,
+      mediaType,
+      draft,
+      ...params
+    } = options;
+
+    expect(currentFixtures.request.method).toEqual(options.method);
+    expect(currentFixtures.request.url).toEqual(options.url);
+
+    Object.keys(params).forEach((paramName) => {
+      expect(currentFixtures.request[paramName]).toStrictEqual(
+        params[paramName]
+      );
+    });
+    return currentFixtures.response;
+  });
+
+  const pr = await octokit.createPullRequest({
+    owner: "gr2m",
+    repo: "pull-request-test",
+    title: "One comes, one goes",
+    body: "because",
+    head: "patch",
+    changes: {
+      files: {
+        "path/to/file1.txt": "Content for file1",
+        "path/to/file2.txt": "Content for file2",
+      },
+      commit: "why",
+    },
+  });
+
+  expect(pr).toStrictEqual(fixturePr);
+  expect(fixtures.length).toEqual(0);
+});
