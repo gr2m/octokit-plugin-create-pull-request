db6b15fa1522a2426b88edbd0c20041065a33776
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-03-03 15:03:29 -0800

    fix: support blobs in forked repos (#84)

---
+++ test/use-fork-blob.test.ts
@@ -0,0 +1,53 @@
+import { Octokit as Core } from "@octokit/core";
+
+import { createPullRequest } from "../src";
+const Octokit = Core.plugin(createPullRequest);
+
+test("use fork blob", async () => {
+  const fixtures = require("./fixtures/use-fork-blob");
+  const octokit = new Octokit();
+
+  octokit.hook.wrap("request", (_, options) => {
+    const currentFixtures = fixtures.shift();
+    const {
+      baseUrl,
+      method,
+      url,
+      request,
+      headers,
+      mediaType,
+      draft,
+      ...params
+    } = options;
+
+    expect(currentFixtures.request.method).toEqual(options.method);
+    expect(currentFixtures.request.url).toEqual(options.url);
+
+    Object.keys(params).forEach((paramName) => {
+      expect(currentFixtures.request[paramName]).toStrictEqual(
+        params[paramName]
+      );
+    });
+    return currentFixtures.response;
+  });
+
+  await octokit.createPullRequest({
+    owner: "gr2m",
+    repo: "pull-request-test",
+    title: "Fork has already been created",
+    body: "because",
+    head: "test-branch-rlbes",
+    changes: {
+      files: {
+        "path/to/1x1-black.gif": {
+          // https://css-tricks.com/snippets/html/base64-encode-of-1x1px-transparent-gif/
+          content: "R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=",
+          encoding: "base64",
+        },
+      },
+      commit: "why",
+    },
+  });
+
+  expect(fixtures.length).toEqual(0);
+});
