4c448585f074317331af0f3c527f39cf5ad3d172
Tyler Michael <tyler.michael@getethos.com>
2022-12-06 15:42:32 -0500

    allow passing in author and committer

---
+++ test/create-commits-with-author-and-committer.ts
@@ -0,0 +1,86 @@
+import { Octokit as Core } from "@octokit/core";
+import { RequestError } from "@octokit/request-error";
+
+import { createPullRequest } from "../src";
+const Octokit = Core.plugin(createPullRequest);
+
+test("multiple commits", async () => {
+  const fixtures = require("./fixtures/create-commits-with-author-and-comitter");
+  const fixturePr = fixtures[fixtures.length - 1].response;
+  const octokit = new Octokit();
+
+  octokit.hook.wrap("request", (_, options) => {
+    const currentFixtures = fixtures.shift();
+    const {
+      baseUrl,
+      method,
+      url,
+      request,
+      headers,
+      mediaType,
+      draft,
+      ...params
+    } = options;
+
+    expect(
+      `${currentFixtures.request.method} ${currentFixtures.request.url}`
+    ).toEqual(`${options.method} ${options.url}`);
+
+    Object.keys(params).forEach((paramName) => {
+      expect(currentFixtures.request[paramName]).toStrictEqual(
+        params[paramName]
+      );
+    });
+
+    if (currentFixtures.response.status >= 400) {
+      throw new RequestError("Error", currentFixtures.response.status, {
+        request: currentFixtures.request,
+        headers: currentFixtures.response.headers,
+      });
+    }
+
+    return currentFixtures.response;
+  });
+
+  const pr = await octokit.createPullRequest({
+    owner: "gr2m",
+    repo: "sandbox",
+    title: "One comes, one goes",
+    body: "because",
+    head: "test-branch-" + Math.random().toString(36).substr(2, 5),
+    changes: [
+      {
+        files: {
+          "path/to/file1.txt": "Content for file1",
+          "path/to/file2.txt": "Content for file2",
+        },
+        author: {
+          name: "Author LastName",
+          email: "Author.LastName@acme.com",
+          date: new Date().toISOString(),
+        },
+        committer: {
+          name: "Committer LastName",
+          email: "Committer.LastName@acme.com",
+          date: new Date().toISOString(),
+        },
+        commit: "why",
+      },
+      {
+        files: {
+          "path/to/file1.txt": "New content",
+          "path/to/file4.txt": "Content for file4",
+        },
+        committer: {
+          name: "Committer Smith",
+          email: "Committer.Smith@acme.com",
+          date: new Date().toISOString(),
+        },
+        commit: "Make a fix",
+      },
+    ],
+  });
+
+  expect(pr).toStrictEqual(fixturePr);
+  expect(fixtures.length).toEqual(0);
+});
