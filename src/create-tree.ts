d87fc45e06b5985f37f1d00c0c24585787ba52bf
Stefan Buck <github@stefanbuck.com>
2023-05-15 22:32:03 +0200

    feat: run file functions calls in sequence

--- src/create-tree.ts
+++ src/create-tree.ts
@@ -15,69 +15,74 @@ export async function createTree(
     latestCommitTreeSha,
   } = state;
 
-  const tree = (
-    await Promise.all(
-      Object.keys(changes.files).map(async (path) => {
-        const value = changes.files[path];
+  let tree = [];
 
-        if (value === null) {
-          // Deleting a non-existent file from a tree leads to an "GitRPC::BadObjectState" error,
-          // so we only attempt to delete the file if it exists.
-          try {
-            // https://developer.github.com/v3/repos/contents/#get-contents
-            await octokit.request("HEAD /repos/{owner}/{repo}/contents/:path", {
-              owner: ownerOrFork,
-              repo,
-              ref: latestCommitSha,
-              path,
-            });
+  for (const path of Object.keys(changes.files)) {
+    const value = changes.files[path];
 
-            return {
-              path,
-              mode: "100644",
-              sha: null,
-            };
-          } catch (error) {
-            return;
+    if (value === null) {
+      // Deleting a non-existent file from a tree leads to an "GitRPC::BadObjectState" error,
+      // so we only attempt to delete the file if it exists.
+      try {
+        // https://developer.github.com/v3/repos/contents/#get-contents
+        await octokit.request("HEAD /repos/{owner}/{repo}/contents/:path", {
+          owner: ownerOrFork,
+          repo,
+          ref: latestCommitSha,
+          path,
+        });
+
+        tree.push({
+          path,
+          mode: "100644",
+          sha: null,
+        });
+        continue;
+      } catch (error) {
+        continue;
+      }
+    }
+
+    // When passed a function, retrieve the content of the file, pass it
+    // to the function, then return the result
+    if (typeof value === "function") {
+      let result;
+
+      try {
+        const { data: file } = await octokit.request(
+          "GET /repos/{owner}/{repo}/contents/:path",
+          {
+            owner: ownerOrFork,
+            repo,
+            ref: latestCommitSha,
+            path,
           }
-        }
+        );
 
-        // When passed a function, retrieve the content of the file, pass it
-        // to the function, then return the result
-        if (typeof value === "function") {
-          let result;
+        result = await value(
+          Object.assign(file, { exists: true }) as UpdateFunctionFile
+        );
+      } catch (error) {
+        // @ts-ignore
+        // istanbul ignore if
+        if (error.status !== 404) throw error;
 
-          try {
-            const { data: file } = await octokit.request(
-              "GET /repos/{owner}/{repo}/contents/:path",
-              {
-                owner: ownerOrFork,
-                repo,
-                ref: latestCommitSha,
-                path,
-              }
-            );
+        // @ts-ignore
+        result = await value({ exists: false });
+      }
 
-            result = await value(
-              Object.assign(file, { exists: true }) as UpdateFunctionFile
-            );
-          } catch (error) {
-            // @ts-ignore
-            // istanbul ignore if
-            if (error.status !== 404) throw error;
+      if (result === null || typeof result === "undefined") continue;
+      tree.push(
+        await valueToTreeObject(octokit, ownerOrFork, repo, path, result)
+      );
+      continue;
+    }
 
-            // @ts-ignore
-            result = await value({ exists: false });
-          }
+    tree.push(await valueToTreeObject(octokit, ownerOrFork, repo, path, value));
+    continue;
+  }
 
-          if (result === null || typeof result === "undefined") return;
-          return valueToTreeObject(octokit, ownerOrFork, repo, path, result);
-        }
-
-        return valueToTreeObject(octokit, ownerOrFork, repo, path, value);
-      })
-    )
-  ).filter(Boolean) as TreeParameter;
+  tree = tree.filter(Boolean) as TreeParameter;
 
   if (tree.length === 0) {
     return null;
