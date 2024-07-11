8858c85a2f6f41fc248cddb521d3157e81a88a07
wolfy1339 <webmaster@wolfy1339.com>
2024-07-11 16:17:58 -0400

    build: switch to esbuild and output ESM
    BREAKING CHANGE: Package is now pure ESM

---
+++ scripts/build.mjs
@@ -0,0 +1,79 @@
+import esbuild from "esbuild";
+import { copyFile, readFile, writeFile, rm } from "node:fs/promises";
+import { glob } from "glob";
+
+const sharedOptions = {
+  sourcemap: "external",
+  sourcesContent: true,
+  minify: false,
+  allowOverwrite: true,
+  packages: "external",
+  platform: "neutral",
+  format: "esm",
+  target: "es2022",
+};
+
+async function main() {
+  // Start with a clean slate
+  await rm("pkg", { recursive: true, force: true });
+  // Build the source code for a neutral platform as ESM
+  await esbuild.build({
+    entryPoints: await glob(["./src/*.ts", "./src/**/*.ts"]),
+    outdir: "pkg/dist-src",
+    bundle: false,
+    ...sharedOptions,
+    sourcemap: false,
+  });
+
+  // Remove the types file from the dist-src folder
+  const typeFiles = await glob([
+    "./pkg/dist-src/**/types.js.map",
+    "./pkg/dist-src/**/types.js",
+  ]);
+  for (const typeFile of typeFiles) {
+    await rm(typeFile);
+  }
+
+  const entryPoints = ["./pkg/dist-src/index.js"];
+
+  await esbuild.build({
+    entryPoints,
+    outdir: "pkg/dist-bundle",
+    bundle: true,
+    ...sharedOptions,
+  });
+
+  // Copy the README, LICENSE to the pkg folder
+  await copyFile("LICENSE.md", "pkg/LICENSE.md");
+  await copyFile("README.md", "pkg/README.md");
+
+  // Handle the package.json
+  let pkg = JSON.parse((await readFile("package.json", "utf8")).toString());
+  // Remove unnecessary fields from the package.json
+  delete pkg.scripts;
+  delete pkg.prettier;
+  delete pkg.release;
+  await writeFile(
+    "pkg/package.json",
+    JSON.stringify(
+      {
+        ...pkg,
+        files: ["dist-*/**", "bin/**"],
+        types: "dist-types/index.d.ts",
+        exports: {
+          ".": {
+            types: "./dist-types/index.d.ts",
+            import: "./dist-bundle/index.js",
+            // Tooling currently are having issues with the "exports" field when there is no "default", ex: TypeScript, eslint
+            default: "./dist-bundle/index.js",
+          },
+        },
+        module: "dist-src/index.js",
+        sideEffects: false,
+      },
+      null,
+      2,
+    ),
+  );
+}
+main();
